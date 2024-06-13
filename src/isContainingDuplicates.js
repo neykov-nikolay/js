01d226b10f83aa0f3eebcf08ee8a7cf62bd73d41
master <teodora.zaharieva@proton.me>
2024-06-13 16:04:50 +0300

    some message

--- src/isContainingDuplicates.js
+++ src/isContainingDuplicates.js
@@ -1,1 +1,20 @@
-export const isContainingDuplicates = (array = []) => {};
+// export const isContainingDuplicates = (array = []) => {
+//   let hasDuplicates = false;
+//   const tempArr = [];
+
+//   array.forEach((element) => {
+//     if (!tempArr.includes(element)) {
+//       tempArr.push(element);
+//     } else {
+//       hasDuplicates = true;
+//     }
+//   });
+
+//   return hasDuplicates;
+// };
+
+export const isContainingDuplicates = (array = []) => {
+  const uniqueArray = [...new Set(array)];
+
+  return array.length > uniqueArray.length;
+};
